94e50f2abf097b27a8da9bc66d3a1264164fbe84
VasaOfficial <VasaPofficial@gmail.com>
2023-02-02 22:17:53 +0100

    first commit

---
+++ src/Pages/NotFount.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import { Link } from 'react-router-dom';
+
+function NotFound() {
+  return (
+    <>
+      <h1>Not Found</h1>
+      <Link to="/">Go Home</Link>
+    </>
+  );
+}
+
+export default NotFound;
