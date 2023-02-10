4abeef55796ffb3fc1dccc1cbb5322df6df38f08
VasaOfficial <VasaPofficial@gmail.com>
2023-02-10 20:38:45 +0100

    add server

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,24 @@
 /* eslint-disable no-console */
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
+
+const products = require('./products');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, '../../Instrument-Store/src')));
 
 app.get('/', (req, res) => {
-  res.send('Welcome to server');
+  res.sendFile(path.join(__dirname, '../../Instrument-Store/src', 'main.jsx'));
 });
 
 app.get('/products', (req, res) => {
-  res.send([2, 3, 4]);
+  res.send(products);
 });
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, console.log(`server is running on port ${port}`));
+app.listen(port, () => console.log(`server is running on port ${port}`));
---
+++ backend/products.js
@@ -0,0 +1,9 @@
+const products = [
+  {
+    id: 1,
+    name: 'electric guitar',
+    price: 999,
+  },
+];
+
+module.exports = products;
