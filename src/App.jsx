8c4aef3bb86afa57a749ef8f3fd8c97195d64454
VasaOfficial <VasaPofficial@gmail.com>
2023-02-14 20:05:08 +0100

    implement lazy and suspense to boost web performance

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
 import { Route, Routes } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
 
-import HomePage from './Pages/Home/index';
 import NotFound from './Pages/NotFount';
-import LandingPage from './Pages/Landing/index';
-import StringedShop from './Pages/Shop/Stringed';
-import WindShop from './Pages/Shop/Wind';
-import PercussionShop from './Pages/Shop/Percussion';
-import CartPage from './Pages/Cart';
+
+const Home = lazy(() => import('./Pages/Home/index'));
+const LandingPage = lazy(() => import('./Pages/Landing/index'));
+const StringedShop = lazy(() => import('./Pages/Shop/Stringed'));
+const WindShop = lazy(() => import('./Pages/Shop/Wind'));
+const PercussionShop = lazy(() => import('./Pages/Shop/Percussion'));
+const CartPage = lazy(() => import('./Pages/Cart/index'));
 
 export function App() {
   return (
-    <Routes>
-      <Route exact path="/" element={<LandingPage />} />
-      <Route path="/home" element={<HomePage />} />
-      <Route path="/stringed" element={<StringedShop />} />
-      <Route path="/wind" element={<WindShop />} />
-      <Route path="/percussion" element={<PercussionShop />} />
-      <Route path="/cart" element={<CartPage />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <Suspense callback={LandingPage}>
+      <Routes>
+        <Route exact path="/" element={<LandingPage />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/stringed" element={<StringedShop />} />
+        <Route path="/wind" element={<WindShop />} />
+        <Route path="/percussion" element={<PercussionShop />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Suspense>
   );
 }
